f7f0b9c2339233f1d8021d9e280b9f951a2942d2
Prashant Maurya <prashant102510@gmail.com>
2026-09-15 22:40:10 +0530

    add the loops statment

---
+++ Javascript/01.Basics/01-Basics/string.js
@@ -0,0 +1,15 @@
+// Switch
+let x = "0";
+let text;
+switch (x) {
+  case 0:
+    text = "off";
+    console.log(bulb);
+    break;
+  case 1:
+    bulb = "On";
+    console.log(bulb);
+    break;
+    defalut: bulb = "no bulb value found ";
+    console.log(bulb);
+}
